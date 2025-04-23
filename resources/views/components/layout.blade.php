54ab89b1eb010ef7090ae5fe4ef297e5da2284b1
Giulio Bitetto <raven.dusky@icloud.com>
2025-04-23 10:33:55 +0200

    Initialize project scaffolding, create base components, and implement preliminary navbar

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Blackbox</title>
+        @vite(['resources/css/app.css'])
+    </head>
+    <body>
+        <x-navbar />
+        {{ $slot }}
+    </body>
+</html>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<div class="terminal-nav">
+    <div class="terminal-logo">
+        <div class="logo terminal-prompt"><a href="{{ route('homepage') }}" class="no-style">Blackbox</a></div>
+    </div>
+    <nav class="terminal-menu">
+        <ul>
+            <li><a class="menu-item active" href="{{ route('homepage') }}">Homepage</a></li>
+            <li><a class="menu-item" href="">Login</a></li>
+            <li><a class="menu-item" href="">Register</a></li>
+        </ul>
+    </nav>
+</div>
